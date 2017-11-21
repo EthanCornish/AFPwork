1470ba039b36911ece81167bef4e2d6a49330c3c
19ecornish <19ecornish@mlmc.vic.edu.au>
2017-11-21 12:37:49 +1100

    string formatting

--- test1.py
+++ test1.py
@@ -1,8 +1,20 @@
 print("Hello World")
 
-x = 0
-while x < 11:
-    print(x)
-    x += 1
+name = input('You?')
+print(name)
 
-print(x)
+age = int(input(print("Age?")))
+
+
+#The comma simply outputs the result
+print('Hello', name, age)
+
+#The + tries to contruct a string to display
+#and it will only work with int if they are
+#converted to string first
+print('Hello' + name + str(age))
+
+
+#The {x} are placeholders. It grahs from the variables in the format
+#Written how you want the code to work
+print('Hello {0} you are {1} years old'.format(name,age))
