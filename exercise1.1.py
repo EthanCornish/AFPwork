0c294b0f3f7f39329acb2376571df61a94c3ba86
19ecornish <19ecornish@mlmc.vic.edu.au>
2017-11-21 18:15:11 +1100

    first commit of exersise1.1

---
+++ exercise1.1.py
@@ -0,0 +1,31 @@
+
+#find % of GCs
+
+#Tells the user what the program does
+print("This program will tell the user the GC content of a DNA sequence")
+
+#the sequence to count
+sequence = 'ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT'
+
+#this asks the user for the code
+sequence = input(print('What is your DNA code?'))
+
+
+
+#counts the number of C's and return s
+numC = sequence.count('C')
+print('C =', numC)
+
+#counts the number of G's and returns
+numG = sequence.count('G')
+print('G =', numG)
+
+#finds how many characters are in the code
+length = len(sequence)
+print('length =', length)
+
+#Calculates the percentage
+percentGC = (((numC + numG)/length) * 100)
+
+#the round part rounds it too 2 decimal points
+print('The GC content of the sequence is {0}%.'.format(round(percentGC, 2)))
