982e3eeefde998f25b2807f2519b5d1f1611028b
19ecornish <19ecornish@mlmc.vic.edu.au>
2017-11-23 12:00:32 +1100

    Completed exercise 1.3

--- exercise1.3.py
+++ exercise1.3.py
@@ -1,18 +1,17 @@
 
 # Restriction Fragment Lengths
-
 print("This program will cut the DNA sequence at 'G*AATTC' and provide the size of each fragment.")
 
 #The sequence
 sequence = 'ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT'
 
 #Finds the index value of where the program should cut
-cutpoint= (sequence.find('GAATTC'))
+cutpoint = (sequence.find('GAATTC'))
 
 #Finds the size of the first fragment
 size1 = cutpoint + 1
 #Finds the size of the second fragment
-size2 = (len(sequence) - 22)
+size2 = (len(sequence) - size1)
 
 #len(sequence) = the total length
 
