242f2a1b7066189332881f890d19b3074b7570a6
19ecornish <19ecornish@mlmc.vic.edu.au>
2017-11-23 11:59:32 +1100

    Completed exercise 1.3

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # AFPwork
 Work done during AFP
 
-AFP Lesson 1 (21/11)
-    EditingInPycharmTest1
+Testing was carried out in the setup of GitHub
+Exercise1.1 was the GC Content of the DNA Sequencing Tasks
+Exercise1.2 was the Complementing DNA Task of the DNA Sequencing Tasks
+Exercise1.3 was the Restriction Fragment Lengths Task of the DNA Sequencing Tasks
---
+++ exercise1.3.py
@@ -0,0 +1,20 @@
+
+# Restriction Fragment Lengths
+
+print("This program will cut the DNA sequence at 'G*AATTC' and provide the size of each fragment.")
+
+#The sequence
+sequence = 'ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT'
+
+#Finds the index value of where the program should cut
+cutpoint= (sequence.find('GAATTC'))
+
+#Finds the size of the first fragment
+size1 = cutpoint + 1
+#Finds the size of the second fragment
+size2 = (len(sequence) - 22)
+
+#len(sequence) = the total length
+
+#Gives the final output
+print('The first fragment is {0} bases long.\nThe second fragment is {1} bases long.'.format(size1,size2))
