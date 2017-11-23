2de8b2238185019c55f058ad203ba6cb3b80b83a
19ecornish <19ecornish@mlmc.vic.edu.au>
2017-11-23 12:41:46 +1100

    Completed exercise 2.1

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Work done during AFP
 
 Testing was carried out in the setup of GitHub
-Exercise1.1 was the GC Content of the DNA Sequencing Tasks
-Exercise1.2 was the Complementing DNA Task of the DNA Sequencing Tasks
-Exercise1.3 was the Restriction Fragment Lengths Task of the DNA Sequencing Tasks
-Exercise1.4 was the Splicing out Introns Task of the DNA Sequencing Tasks
+Exercise1.1 was the GC Content of the Text Manipulation Tasks
+Exercise1.2 was the Complementing DNA Task of the Text Manipulation Tasks
+Exercise1.3 was the Restriction Fragment Lengths Task of the Text Manipulation Tasks
+Exercise1.4 was the Splicing out Introns Task of the Text Manipulation Tasks
+Exercise2.1 was the Splitting Genomeic DNA of the Handling Files Tasks
---
+++ exercise2.1.py
@@ -0,0 +1,21 @@
+
+# Splitting Genomic DNA and Writing to a File
+
+### The following code is taken from 1.4
+# The sequence being used
+sequence = 'ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCG' \
+           'ATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT'
+
+# Finding the two exons,    notation used is 'string[start:end]' note start is inclusive, end is not
+exon1 = sequence[0:65]
+exon2 = sequence[91:len(sequence)]
+
+### The following code is new
+
+exercise21fileA = open('exercise2.1FileA', 'w')
+exercise21fileA.write('The first exon is {0}'.format(exon1))
+exercise21fileA.close()
+
+exercise21fileB = open('exercise2.1FileB', 'w')
+exercise21fileB.write('The second exon is {0}'.format(exon2))
+exercise21fileB.close()
